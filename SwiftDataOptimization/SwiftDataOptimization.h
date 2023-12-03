c9dc783bac215bc4fea25b989488e9fa8ea5d75d
Rutu Patel <patelrutvij2801@gmail.com>
2023-12-03 11:52:59 -0800

    First commit

---
+++ SwiftDataOptimization/SwiftDataOptimization.h
@@ -0,0 +1,18 @@
+//
+//  SwiftDataOptimization.h
+//  SwiftDataOptimization
+//
+//  Created by Rutvij Patel on 12/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDataOptimization.
+FOUNDATION_EXPORT double SwiftDataOptimizationVersionNumber;
+
+//! Project version string for SwiftDataOptimization.
+FOUNDATION_EXPORT const unsigned char SwiftDataOptimizationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDataOptimization/PublicHeader.h>
+
+
